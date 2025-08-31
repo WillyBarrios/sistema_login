4364b31250e49b79af465bac0d65d959c7e98139
Wbarrios <43481188+WillyBarrios@users.noreply.github.com>
2025-08-31 10:30:08 -0600

    Update configuration values in config.py and add sistema_login.zip

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "contraseña")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://tu-usario:contraseña@localhost/sistemaventa")
+    SECRET_KEY = os.getenv("SECRET_KEY", "Aa12345678")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "mysql+pymysql://wbarrios:Aa12345678@localhost/sistemaventa")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "contraseña")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "Aa12345678")
